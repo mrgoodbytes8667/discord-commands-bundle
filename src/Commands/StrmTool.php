77b0884fc824593abbee05b4399a23ec0acc8944
MrGoodBytes <bytes@goodbytes.live>
2021-09-15 15:04:01 -0500

    Add new functions from interface

--- src/Commands/StrmTool.php
+++ src/Commands/StrmTool.php
@@ -6,6 +6,7 @@ namespace Bytes\DiscordCommandsBundle\Commands;
 
 use Bytes\DiscordClientBundle\Slash\SlashCommandInterface;
 use Bytes\DiscordResponseBundle\Enums\ApplicationCommandOptionType as ACOT;
+use Bytes\DiscordResponseBundle\Enums\Permissions;
 use Bytes\DiscordResponseBundle\Objects\Application\Command\ChatInputCommand;
 use Bytes\DiscordResponseBundle\Objects\Slash\ApplicationCommand;
 use Bytes\DiscordResponseBundle\Objects\Slash\ApplicationCommandOption as Option;
@@ -36,5 +37,23 @@ class StrmTool implements SlashCommandInterface
     public static function getDefaultIndexName(): string
     {
         return 'strmtool';
+    }
+
+    /**
+     * @return bool False will prevent the permissions command from displaying the @everyone role.
+     */
+    public static function allowEveryoneRole(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Returns an array of Discord Permission enums that will be used by the add command to build a list of roles to get
+     * access to run the command. Any role that has any permission in this list will get access.
+     * @return Permissions[]
+     */
+    public static function getPermissionsForRoles(): array
+    {
+        return [];
     }
 }
