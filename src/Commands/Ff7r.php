55ab8e2530c4d06a981d89df16333b2a903d5fbe
MrGoodBytes <bytes@goodbytes.live>
2021-09-16 12:12:09 -0500

    Option names must be lowercase

--- src/Commands/Ff7r.php
+++ src/Commands/Ff7r.php
@@ -22,8 +22,8 @@ class Ff7r implements SlashCommandInterface
      */
     public static function createCommand(): ApplicationCommand
     {
-        return ChatInputCommand::createChatCommand('ff7r', 'How long will it be until Final Fantasy VII Remake releases?', [
-            Option::create(ACOT::string(), 'Version', 'Which part of the remake?', false, [
+        return ChatInputCommand::createChatCommand(static::getDefaultIndexName(), 'How long will it be until Final Fantasy VII Remake releases?', [
+            Option::create(ACOT::string(), 'version', 'Which part of the remake?', false, [
                 ApplicationCommandOptionChoice::create('Part 1'),
                 ApplicationCommandOptionChoice::create('Part 1 Intergrade'),
                 ApplicationCommandOptionChoice::create('Part 2')
